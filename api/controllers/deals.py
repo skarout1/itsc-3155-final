639eb0505ce2a8bcb92ff6b26060b10a3ba1d844
qrconroy <qrconroy@gmail.com>
2026-04-29 15:01:07 -0400

    feat: created/completed deals and purchase_history controllers

---
+++ api/controllers/deals.py
@@ -0,0 +1,78 @@
+import uuid
+from datetime import datetime
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import orders as model
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def create(db: Session, request):
+    new_item = model.Deals(
+        product_id=request.product_id,
+        promo_code=request.promo_code,
+        description=request.description,
+        deal_type=request.deal_type,
+        discount_percent=request.discount_percent,
+        buy_quantity=request.buy_quantity,
+        get_quantity=request.get_quantity,
+        expiration_date=request.expiration_date,
+        created_at=request.created_at
+
+    )
+
+    try:
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    return new_item
+
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.Deals).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
+
+
+def read_one(db: Session, item_id):
+    try:
+        item = db.query(model.Deals).filter(model.Deals.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item
+
+
+def update(db: Session, item_id, request):
+    try:
+        item = db.query(model.Deals).filter(model.Deals.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item.first()
+
+
+def delete(db: Session, item_id):
+    try:
+        item = db.query(model.Deals).filter(model.Deals.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
