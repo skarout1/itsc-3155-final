5c0c49b6fbb4d62811e25cef3df61614a4359bc6
Wes Packham <wesleypackham@gmail.com>
2026-05-03 04:21:37 -0400

    feat: Wire all existing routers into index.py

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,6 +1,13 @@
-from . import orders, order_details
+from . import orders, order_details, deals, product, purchase_history, recipes, resources, reviews, sandwiches
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(deals.router)
+    app.include_router(product.router)
+    app.include_router(purchase_history.router)
+    app.include_router(recipes.router)
+    app.include_router(resources.router)
+    app.include_router(reviews.router)
+    app.include_router(sandwiches.router)
